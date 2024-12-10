df24778b363b3dad713fe1485405afc32155308b
Rohit Yadav <rohityadav9769@gmail.com>
2024-12-10 21:33:54 +0530

    changes done!

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import "./styles/common/reset.css"
-import Accordian from './components/accordian/accordian'
+import Accordian from './components/accordian/Accordian'
 import { IMAGE_PATH } from "./constant/Image"
 
 function App() {
--- src/constant/Image.js
+++ src/constant/Image.js
@@ -1,1 +1,2 @@
-export const IMAGE_PATH = "/assest/Images"
+export const IMAGE_PATH = `${import.meta.env.BASE_URL}assest/Images`
+// export const IMAGE_PATH = "/assest/Images"
